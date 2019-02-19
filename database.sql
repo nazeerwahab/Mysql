1c5552f54549f48d9dc70c488f98ca6b3b2d9e87
nazeerwahab <42862748+nazeerwahab@users.noreply.github.com>
2019-02-19 12:28:57 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,13 @@
+create DATABASE guvi1;
+use guvi1;
+create table table1(
+sno int(3),
+name char(20),
+age int(3),
+gender char(8),
+address varchar(80),
+mobile_num int(13),
+email varchar(20),
+dept char(8),
+semester varchar(10),
+no_arrears int(30));
